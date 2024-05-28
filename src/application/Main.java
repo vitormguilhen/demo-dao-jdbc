be28db52e7d2ff39c8eaea4060b17c6c7e17e468
Vitor Machado <vitormguilhen@gmail.com>
2024-05-28 10:06:55 -0300

    Department entity class

---
+++ src/application/Main.java
@@ -0,0 +1,10 @@
+package application;
+
+import model.entities.Department;
+
+public class Main {
+    public static void main(String[] args) {
+        Department obj = new Department(1, "Book");
+        System.out.println(obj );
+    }
+}
