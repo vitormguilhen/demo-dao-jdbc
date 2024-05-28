a6a3a4c1c703e3f7045ece406d7704e90cb39833
Vitor Machado <vitormguilhen@gmail.com>
2024-05-28 10:33:26 -0300

    DaoFactory DaoJDBC

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -8,6 +10,7 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         Department obj = new Department(1, "Book");
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
         System.out.println(seller );
     }
